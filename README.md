ab104161bcb97654f959b3fcc89f7ca91f2c4ce9
ImFuryPro <imfurypro@gmail.com>
2021-11-03 17:44:08 +0400

    add new native version live translate

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# justLiveTranslate-v2
-justLiveTranslate-v2
+# Just Live Translate v2.0
+Live Dictionary Translate<br />
+
+Author: Иван "ImFuryPro" Сапоненко<br />
+Version: 2.0<br />
+Description: Реализует живой перевод сайта по словарю. Версия без использования jQuery.<br />
+
+Версия с JQuery: [https://github.com/ImFuryPro/JustLiveTranslate](https://github.com/ImFuryPro/JustLiveTranslate)
+
+## Использование
+1. Устанавливаем необходимый словарь по пути **/js/dictionaries/dictionary.json**;
+2. Устанавливаем скрипт в код страницы, перед тегом body;
+3. Для перевода слов, в необходимых тегах устанавливаем атрибут [data-word="*"], где * - ключ текста в словаре (п.1);
+4. Для установки кнопки для смены языка, необходимо в тег ссылок или кнопки установить атрибуты [data-action="changeLang"] и [data-lang="*"], где * - ключ языка в словаре (п.1);
+5. ...
+6. Profit!
+
+**ImFuryPro**, 2021 (c)
---
+++ js/liveTranslate.js
@@ -0,0 +1,65 @@
+/*
+* Just Live Translate Native
+* Author: Иван "ImFuryPro" Сапоненко
+* Version: 2.0
+* Description: Реализует живой перевод сайта по созданному словарю. Версия без использования jQuery.
+*/
+
+let newSource = './js/dictionaries/dictionary.json', // Путь до словаря в формате JSON
+    changeLangButtons = document.querySelectorAll(`[data-action="changeLang"]`); // Получаем все кнопки для смены языка
+
+// По умолчанию "Русский"
+if (localStorage.getItem("lang") === "") {
+    localStorage.setItem("lang", "ru");
+}
+
+// Читаем словарь
+function readTextFile(file) {
+    let rawFile = new XMLHttpRequest(),
+        allText = "";
+
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                allText = rawFile.responseText;
+            }
+        }
+    }
+    rawFile.send(null);
+    return allText;
+}
+
+/*
+* Получение словаря из JSON файла и его обработка
+* source - путь до словаря
+* lang - язык словаря
+*/
+function getSetDictionary(source, lang) {
+    let dictionary = readTextFile(source),
+        parseDictionary = JSON.parse(dictionary),
+        langArray = parseDictionary[lang];
+
+    localStorage.setItem("lang", lang); // Устанавливаем в хранилище значение языка, чтобы при обновлении страницы язык не сменился
+
+    Object.keys(langArray).map(function(arrayObjKey) {
+        let domElement = document.querySelector(`[data-word="${arrayObjKey}"]`);
+
+        if (domElement.tagName === "INPUT") {
+            domElement.setAttribute("placeholder", langArray[arrayObjKey]);
+        } else {
+            domElement.innerHTML = langArray[arrayObjKey];
+        }
+    });
+}
+
+// Вызываем функцию сразу, как только страница загрузилась
+getSetDictionary(newSource, localStorage.getItem("lang"));
+
+// Переключение языка
+changeLangButtons.forEach(button => {
+    button.addEventListener("click", function (e) {
+        e.preventDefault();
+        getSetDictionary(newSource, this.dataset.lang);
+    });
+});
